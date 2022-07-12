4389e74b327f705b3dc9f648ef663dc13a23e589
MitchellQ <mitchell.quarrie@outlook.com>
2022-07-12 21:40:49 +1200

    Completed first half of tutorial
     set up app before adding flavors

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'resources/display_strings.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +13,36 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flavors Example'),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+      child: Column(
+        children: <Widget>[
+          const Text(appTitle),
+          Text(date + getDateForDisplay()),
+          const Text(appDescription),
+          Image.asset(
+            'assets/dancing.png',
+            width: 50.0,
+            height: 50.0,
+          ),
+          Image.asset('assets/1.png', width: 50.0, height: 50.0),
+        ],
+      ),
+    );
+  }
+
+  String getDateForDisplay() {
+    DateTime now = DateTime.now();
+    var formatter = DateFormat('EEEE dd MMMM yyyy');
+    return formatter.format(now);
   }
 }
---
+++ lib/resources/display_strings.dart
@@ -0,0 +1,3 @@
+const String appTitle = 'SingleApp';
+const String date = 'Today is ';
+const String appDescription = 'Single App Description blah blah blah';
